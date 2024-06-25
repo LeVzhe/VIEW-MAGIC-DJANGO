a3558984c4dc2a70eaa16aa141f4fd568a82c950
Siarhei Zaluzhniy <levzh93@gmail.com>
2024-06-25 22:49:21 +0300

    feat: add model

--- main_window/admin.py
+++ main_window/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import ContentDb
 
-# Register your models here.
+admin.site.register(ContentDb)
--- main_window/models.py
+++ main_window/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class ContentDb(models.Model):
+    name_field = models.CharField(max_length=20, verbose_name='Название')
+    path_field = models.CharField(max_length=20, verbose_name='Папка')
+    url_field = models.URLField(verbose_name='Адрес')
+    image_field = models.ImageField(upload_to='main_window/db_content/img/', verbose_name='Фото')
+    file_field = models.FileField(upload_to='main_window/db_content/videos/', verbose_name='Видео')
+    description_field = models.TextField(verbose_name='Информация')
+    date_time = models.DateTimeField(auto_now=True, verbose_name='Время загрузки')
