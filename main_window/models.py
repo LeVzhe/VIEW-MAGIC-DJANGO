a60e3265d6f13bd27dd2eb276b1580d21330a938
Siarhei Zaluzhniy <levzh93@gmail.com>
2024-06-27 00:09:54 +0300

    feat: add uuid named photos

--- main_window/models.py
+++ main_window/models.py
@@ -1,8 +1,17 @@
 from django.db import models
+from os.path import splitext
+from uuid import uuid4
+from django.core.files.storage import FileSystemStorage
+
+class UUIDFileStorage(FileSystemStorage):
+    def get_available_name(self, name, max_length=None):
+        _, ext = splitext(name)
+        return 'main_window/db_content/img/' + uuid4().hex + ext
+
 
 class PhotosContent(models.Model):
     user_name = models.CharField(max_length=20, verbose_name='Название')
-    load_img = models.ImageField(upload_to='main_window/db_content/img/', verbose_name='Фото')
+    load_img = models.ImageField(upload_to='main_window/db_content/img/', verbose_name='Фото', storage=UUIDFileStorage())
     description = models.TextField(verbose_name='Информация')
     create_time = models.DateTimeField(auto_now=True, verbose_name='Время создания')
     is_hidden = models.BooleanField(verbose_name='Поле скрыто', default=False)
